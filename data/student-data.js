dcc10f854df4af95cab9a0e21470d550d259c696
Johny270 <jeantakam56@yahoo.com>
2023-09-14 11:01:17 -0300

    Complete lab

---
+++ data/student-data.js
@@ -0,0 +1,9 @@
+const students = [
+  {studentName: 'Jonathan Takam', isStudent: true, _id: 125223},
+  {studentName: 'Erwin Smith', isStudent: false, _id: 127904},
+  {studentName: 'Mikasa Ackerman', done: false, _id: 139608},
+]
+
+export { 
+	students
+}
